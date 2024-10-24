0aa624b94d20a96543b75a9ad66e3f7f6fa03f31
sake-2003 <sake_2003@hotmail.com>
2024-10-23 21:14:06 -0600

    changed .gitignore

--- obj/Debug/net8.0/SeleniumNUnitCsharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SeleniumNUnitCsharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumNUnitCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb254b2540437d8da56b07ed7ac7db80930f6117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72e59991817f9953561bc876c31a74ec7f9c4e40")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumNUnitCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumNUnitCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
